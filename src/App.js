6d8fbcdf067b974d60fd57fbca22b8767f90cbd2
Razi <raziazam.n@gmail.com>
2022-07-13 21:22:14 +0530

    Second React Based Shopping Site Capstone Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import CategoryBanner from './banner/CategoryBanner';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import ProductList from './product-list/ProductList';
+import ProductDetails from './product-details/ProductDetails';
+import Cart from './cart/Cart';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+        <Routes>
+          <Route path='/' element={<ProductList />} />
+          <Route path='/product-details/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      <Footer />
+    </>
   );
 }
 
